5b33fae5ba42ffefde74a20ca4a726421dbb39aa
janpozywilko <jasui99@gmail.com>
2022-12-02 16:26:59 +0200

    structural change

--- day_one/script.py
+++ day_one/script.py
@@ -1,9 +1,25 @@
-with open('data.txt') as data:
-    elf_calories = data.read().split('\n\n')
+import typer
 
 
-for idx ,elf in enumerate(elf_calories):
-    calories_str = elf.split('\n')
-    elf_calories[idx] = sum([int(cal) for cal in calories_str])
+
+def part_one():
+    with open('data.txt') as data:
+        elf_calories = data.read().split('\n\n')
+
+
+    for idx ,elf in enumerate(elf_calories):
+        calories_str = elf.split('\n')
+        elf_calories[idx] = sum([int(cal) for cal in calories_str])
+
+    return max(elf_calories)
+
+def part_two():
+    pass
    
-print(max(elf_calories))
+
+def main():
+    part_one_res = part_one()
+    print(part_one_res)
+
+if __name__ == "__main__":
+    typer.run(main)
