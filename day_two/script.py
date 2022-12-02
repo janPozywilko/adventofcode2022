35533771ef05337895b13a751ccf485b3f7ecb31
janpozywilko <jasui99@gmail.com>
2022-12-02 17:25:41 +0200

    part two

--- day_two/script.py
+++ day_two/script.py
@@ -3,6 +3,8 @@ import typer
 shapes_value = {'X': 1, 'Y': 2, 'Z': 3}
 shapes_conversion = {'X': 'A', 'Y': 'B', 'Z': 'C'}
 
+result_conversion = {'A X': 3, 'A Y': 1 + 3, 'A Z': 2 + 6, 'B X': 1, 'B Y': 2 + 3, 'B Z': 3 + 6, 'C X': 2, 'C Y': 3 + 3, 'C Z': 1 + 6}
+
 test = '''A Y
 B X
 C Z'''
@@ -28,15 +30,22 @@ def part_one():
     return score
 
 def part_two():
-    pass
+    with open('data.txt') as f:
+        data = f.read().split('\n')
+
+    score = 0
+
+    for row in data:
+        score += result_conversion.get(row)
+
+    return score
 
 
 def main():
     part_one_res = part_one()
     print(f'Part one result: {part_one_res}')
-    #part_two_res = part_two()
-    #print(f'Part two result: {part_two_res}')
-    print()
+    part_two_res = part_two()
+    print(f'Part two result: {part_two_res}')
 
 
 if __name__ == "__main__":
