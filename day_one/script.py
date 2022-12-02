f82d74e4f00e23c91ad1eb6c527d5b25191de8a6
janpozywilko <jasui99@gmail.com>
2022-12-02 16:40:47 +0200

    part two result

--- day_one/script.py
+++ day_one/script.py
@@ -14,12 +14,25 @@ def part_one():
     return max(elf_calories)
 
 def part_two():
-    pass
+    with open('data.txt') as data:
+        elf_calories = data.read().split('\n\n')
+
+
+    for idx ,elf in enumerate(elf_calories):
+        calories_str = elf.split('\n')
+        elf_calories[idx] = sum([int(cal) for cal in calories_str])
+    
+    elf_calories.sort()
+
+    return sum(elf_calories[-3:])
    
 
 def main():
     part_one_res = part_one()
-    print(part_one_res)
+    print(f'Part one result: {part_one_res}')
+    part_two_res = part_two()
+    print(f'Part two result: {part_two_res}')
+
 
 if __name__ == "__main__":
     typer.run(main)
