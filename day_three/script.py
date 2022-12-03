8bff4dcebb1ed094d458682c025994be37d76589
janpozywilko <jasui99@gmail.com>
2022-12-03 20:47:39 +0200

    day three second

--- day_three/script.py
+++ day_three/script.py
@@ -1,4 +1,5 @@
 import typer
+import re
 
 test = '''vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
@@ -12,8 +13,11 @@ def find_unique(first_list, second_list):
     unique = list(set(first_list).intersection(second_list))
     return unique[0]
 
+def find_unique_three(first_list, second_list, third_list):
+    unique = list(set(first_list).intersection(second_list, third_list))
+    return unique[0]
+
 def calculate_value(string_value):
-    
     ''' This function should return integer value converted from string with rule: Lowercase item types a through z have priorities 1 through 26, Uppercase item types A through Z have priorities 27 through 52'''
 
     value = 0
@@ -38,14 +42,24 @@ def part_one():
     return sum
 
 def part_two():
-    pass
+    with open('data.txt') as f:
+        data = f.read()
+    data = re.findall('((?:[^\n]+\n?){1,3})', data)
+    unique = []
+    sum = 0
+    for row in data:
+        first_rucksack, second_rucksack, third_rucksack = row.split('\n')[:3]
+        unique_val = find_unique_three(first_rucksack, second_rucksack, third_rucksack)
+        sum += calculate_value(unique_val)
+        unique.append(unique_val)
+    return sum
 
 
 def main():
     part_one_res = part_one()
     print(f'Part one result: {part_one_res}')
-    #part_two_res = part_two()
-    #print(f'Part two result: {part_two_res}')
+    part_two_res = part_two()
+    print(f'Part two result: {part_two_res}')
 
 
 if __name__ == "__main__":
