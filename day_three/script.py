ce9b5fd80efbb3b48fe0b11659e13861c8f5feda
janpozywilko <jasui99@gmail.com>
2022-12-03 10:14:57 +0200

    initial commit

---
+++ day_three/script.py
@@ -0,0 +1,19 @@
+import typer
+
+def part_one():
+    pass
+
+def part_two():
+    pass
+
+
+def main():
+    #part_one_res = part_one()
+    #print(f'Part one result: {part_one_res}')
+    #part_two_res = part_two()
+    #print(f'Part two result: {part_two_res}')
+    print()
+
+
+if __name__ == "__main__":
+    typer.run(main)
