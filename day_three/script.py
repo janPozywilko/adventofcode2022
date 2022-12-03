210789badb509ba372393cb66eb8d4321b25c982
janpozywilko <jasui99@gmail.com>
2022-12-03 10:28:02 +0200

    first steps

--- day_three/script.py
+++ day_three/script.py
@@ -1,15 +1,41 @@
 import typer
 
-def part_one():
+test = '''vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw'''
+
+
+def find_unique(first_list, second_list):
+    unique = list(set(first_list).intersection(second_list))
+    return unique[0]
+
+def calculate_value(string_value):
+    
+    ''' This function should return integer value converted from string with rule: Lowercase item types a through z have priorities 1 through 26, Uppercase item types A through Z have priorities 27 through 52'''
+
     pass
+
+def part_one():
+    data = test.split('\n')
+    unique = []
+    sum = 0
+    for row in data:
+        first_compartment, second_compartment = row[:int(len(row)/2)], row[int(len(row)/2):]
+        unique_val = find_unique(first_compartment, second_compartment)
+        #sum += calculate_value(unique_val)
+        unique.append(unique_val)
+    return unique
 
 def part_two():
     pass
 
 
 def main():
-    #part_one_res = part_one()
-    #print(f'Part one result: {part_one_res}')
+    part_one_res = part_one()
+    print(f'Part one result: {part_one_res}')
     #part_two_res = part_two()
     #print(f'Part two result: {part_two_res}')
     print()
